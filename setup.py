fc65558c8aa36d87801b1aeb93fd33520048bb81
tdimitri <thomasdimitri@gmail.com>
2020-09-30 08:52:37 -0500

    change compiler warnings on gcc

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ else:
 if platform.system() == 'Windows':
     CFLAGS += ' /Ox /Ob2 /Oi /Ot /d2FH4-'
 else:
-    CFLAGS += ' -mavx2 -fpermissive -Wno-unused-variable -Wunused-function -std=c++11 -pthread -falign-functions=32'
+    CFLAGS += ' -mavx2 -fpermissive -Wno-unused-variable -Wno-unused-function -std=c++11 -pthread -falign-functions=32'
 
 def read(*names, **kwargs):
     with io.open(
