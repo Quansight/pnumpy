6b9061ff36f5cf70f8769c6046746e4d4e9f5203
mattip <matti.picus@gmail.com>
2020-10-29 07:25:38 +0200

    improve documentation

--- src/fast_numpy_loops/__init__.py
+++ src/fast_numpy_loops/__init__.py
@@ -1,6 +1,30 @@
 """
-Override all the NumPy UFunc loops with multithreaded variants. The original
-loop will be called via a pool of threads.
+Call ``initialize`` to setup the package. This imports and scans NumPy,
+replacing all the inner loops of UFuncs with wrapped versions. Then you can
+enable/disable any of the subsystems:
+
+  - threading
+
+    Threading will kick in when the number of elements to be processed is more
+    than 50,000. It will break the operation into chunks. Each chunk will be
+    executed in its own thread.
+  - ledger
+
+    The ledger (disabled in version 0.1) records data on each loop execution to
+    enable more accurate heuristics on memory allocation, threading behavior
+    and reporting for logging and benchmarking.
+  - recycler
+
+    Once we can change the NumPy memory allocation strategy, we can use the
+    data from the ledger to create more performant memory caches.
+  - atop
+
+    NumPy is making progress with faster inner loops, but an outside package
+    can iterate faster to provide even faster ones. Since the Universal SIMD
+    loops are in a state of flux at this time, this is disabled for version
+    0.1.
+
+
 """
 __version__ = '0.0.0'
 __all__ = [
