bef8c2e4fc4bb24bc09e3e8a8f33b22eb5c9773f
mattip <matti.picus@gmail.com>
2020-12-15 22:56:07 +0200

    move AVX2 test to the initialize_pnumpy fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,15 +6,11 @@ import pnumpy
 old_numpy = Version(np.__version__) < Version('1.18')
 
 
-def pytest_ignore_collect(path, config):
-    from numpy.core._multiarray_umath import __cpu_features__ as cpu
-    if not cpu['AVX2']:
-        import warnings
-        warnings.warn('tests/conftest.py: skipping since pnumpy requires AVX2')
-        return True
-
 @pytest.fixture(scope='session')
 def initialize_pnumpy():
+    from numpy.core._multiarray_umath import __cpu_features__ as cpu
+    if not cpu['AVX2']:
+        pytest.skip('pnumpy.initialize requires AVX2')
     pnumpy.initialize()
 
 @pytest.fixture(scope='function')
