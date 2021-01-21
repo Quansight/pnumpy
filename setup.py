0b29c92b675ee346f3fd35a6c397e57423853c6d
tdimitri <62962217+tdimitri@users.noreply.github.com>
2021-01-21 13:48:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,63 +32,16 @@ except Exception:
     except Exception:
         print("**could not install pip or setuptools_scm, version is defaulted")
 
-from os.path import dirname, isdir, join
-import os
-import re
-import subprocess
-
-version_re = re.compile('^Version: (.+)$', re.M)
-
-
-def get_version2():
-    d = dirname(__file__)
-
-    if isdir(join(d, '.git')):
-        # Get the version using "git describe".
-        cmd = 'git describe --tags --match [0-9]*'.split()
-        try:
-            version = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get version number from git tags')
-            exit(1)
-
-        # PEP 386 compatibility
-        if '-' in version:
-            version = '.post'.join(version.split('-')[:2])
-
-        # Don't declare a version "dirty" merely because a time stamp has
-        # changed. If it is dirty, append a ".dev1" suffix to indicate a
-        # development revision after the release.
-        with open(os.devnull, 'w') as fd_devnull:
-            subprocess.call(['git', 'status'],
-                            stdout=fd_devnull, stderr=fd_devnull)
-
-        cmd = 'git diff-index --name-only HEAD'.split()
-        try:
-            dirty = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get git index status')
-            exit(1)
-
-        if dirty != '':
-            version += '.dev1'
-
-    else:
-        # Extract the version from the PKG-INFO file.
-        with open(join(d, 'PKG-INFO')) as f:
-            version = version_re.search(f.read()).group(1)
-
-    return version
-
 def myversion():
     try:
-        version = get_version()
+        #version = get_version()
+        version = '2.0.11'
     except Exception:
         version = '2.0.11'
     return version
 
 thisversion=myversion()
-thisversion = '.'.join(thisversion.split('.')[:3])
+#thisversion = '.'.join(thisversion.split('.')[:3])
 
 def writeversion():
     text_file = open("src/pnumpy/_version.py", "w")
