9da77e236f939931cebd20861b66104ae0d62e46
tdimitri <62962217+tdimitri@users.noreply.github.com>
2021-01-29 14:14:57 -0600

    v2.0.20

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ except Exception:
         print("**could not install pip or setuptools_scm, version is defaulted")
 
 def myversion():
-    version = '2.0.19'
+    version = '2.0.20'
     try:
         mversion = get_version()
         s = mversion.split('.')
