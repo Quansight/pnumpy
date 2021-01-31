528f64a32050db1c9fc8a1c6be67e1edb2de239d
mattibot <mattigit@picus.org.il>
2021-01-31 17:39:52 +0000

    auto-generating sphinx docs

--- docs/main/_static/documentation_options.js
+++ docs/main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.1',
+    VERSION: 'v2.0.20',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
